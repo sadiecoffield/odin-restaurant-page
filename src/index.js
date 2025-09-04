da3ca132c5fdec0ffb49b5aa8c8871ad8cf4179e
sadiecoffield <sadiec05@yahoo.com>
2025-09-04 20:50:17 +0100

    Switch page content when tab button is clicked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./styles.css";
-import { renderHomePage } from './renderHomePage.js';
+import { renderHomePage } from './homePage.js';
+
+function switchTabs() {
+  const container = document.querySelector('#content');
+  const navButtons = Array.from(document.querySelectorAll('button'));
+
+  navButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      // Clear content container
+      container.innerHTML = '';
+
+      if (btn.textContent === 'Home') {
+        renderHomePage();
+      } else if (btn.textContent === 'Menu') {
+        // render menu module
+      } else {
+        // render about module
+      }
+    });
+  }); 
+}
 
 renderHomePage();
+switchTabs();
